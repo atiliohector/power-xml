53dbeefd990bb9e65d65e792eab5c390c8ef3d70
Atilio <atiliogoulart@gmail.com>
2021-06-30 05:04:59 -0300

    Setup project

--- app/xml_generator/apps.py
+++ app/xml_generator/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class XmlGeneratorConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'xml_generator'
+    name = 'app.xml_generator'
