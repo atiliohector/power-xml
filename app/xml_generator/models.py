eecf9b57dfe38ea7143171bbcd7d845eae2bdcb0
Atilio <atiliogoulart@gmail.com>
2021-06-30 05:16:19 -0300

    Updating models

--- app/xml_generator/models.py
+++ app/xml_generator/models.py
@@ -3,9 +3,9 @@ from django.db import models
 
 class Xml(models.Model):
 
-    information1 = models.CharField('Information 1', blank=False)
-    information2 = models.CharField('Information 2', blank=False)
-    information3 = models.CharField('Information 3', blank=False)
+    information1 = models.CharField('Information 1', blank=False, max_length=255)
+    information2 = models.CharField('Information 2', blank=False , max_length=255)
+    information3 = models.CharField('Information 3', blank=False, max_length=255)
 
 
     def __str__(self):
